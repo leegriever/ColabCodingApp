d9aacde94b623e0e935eb1edfffc12b7494248ae
Lee Griever <leegriever@Lees-MacBook-Pro.local>
2024-02-20 03:13:12 +0200

    URL update

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = 3080;
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'https://colabcodingapp-client.onrender.com',
+    origin: 'https://colabcodingapp-production-9438.up.railway.app/',
     credentials: true
 };
 app.use(cors(corsOptions));
