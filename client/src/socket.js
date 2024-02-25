5a9eb31bfe4fb0cd29e74a78b2ff67be248daa6b
Lee Griever <leegriever@Lees-MacBook-Pro.local>
2024-02-25 23:02:49 +0200

    URL fixed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : "colabcodingapp-production-d42f.up.railway.app";
+const URL = process.env.NODE_ENV === 'production' ? undefined : "https://colabcodingapp-production-9438.up.railway.app";
 
 export const socket = io(URL, {
     autoConnect: false
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = 3080;
 app.use(express.json());
 
 const corsOptions = {
-    origin: "colabcodingapp-production-d42f.up.railway.app",
+    origin: "https://colabcodingapp-production-9438.up.railway.app",
     credentials: true
 };
 app.use(cors(corsOptions));
